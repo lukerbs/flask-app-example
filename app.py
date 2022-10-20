765d447c8b8d1240e595b5536953527d1404c2ee
Luke Kerbs <luke@Lukes-MBP-3.attlocal.net>
2022-10-19 17:42:31 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+port = 8000
+
+
+# home page
+@app.route('/')
+def home():
+	favorite_food = "pizza"
+	return render_template('index.html', food=favorite_food)
+
+# page 2
+@app.route('/page_2')
+def page_2():
+	return render_template('page_2.html')
+
+
+if __name__ == '__main__':
+    # start the app
+    app.run(host='0.0.0.0', port=port, debug=False)
